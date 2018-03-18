be48b8bc3f054cd9116101937145bd05f59213ef
yangxilin <1345213460@qq.com>
2018-03-18 15:15:51 +0800

    新增

--- yxl.js
+++ yxl.js
@@ -39,15 +39,15 @@
 		} 
 		// insertAfter(newElement,targetElement) 
 //4. 随机出现的16进制颜色值:
-      function color(){
-            var arr=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
-            var result="#"
-            for(var i=0;i<6;i++){
+    function color(){
+        var arr=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+        var result="#"
+        for(var i=0;i<6;i++){
             var  num=Math.floor(Math.random()*16);
             result+=arr[num];
-          }
-            return result;
         }
+        return result;
+    }
         // var liList=document.getElementById('top').getElementsByTagName('li')
         //  for (var i=0;i<liList.length;i++){
         //    liList[i].style.backgroundColor=color()
@@ -68,4 +68,43 @@
     			console.log(all.childNodes[a])
     		}
     	}
-    }				
+    }	
+//7.操作cookie的函数：
+    //1.获取到要找的属性在字符串中的起始位startIndex
+    //2.获取到要找的属性所对应的属性值在字符串中的终止位置endIndex
+    //3.从起始位startIndex，截取到终止位endIndex
+    //4.以=为切割点，将第三步截取到的字符串切割为长度2的数组，数组下标[1]就是寻找属性对应的属性值.
+
+    //getCookie函数传一个参数string类型
+    function getCookie(string){
+        var str=document.cookie
+        var startIndex=str.indexOf(string)
+        var endIndex=str.indexOf(";",startIndex)
+        if(endIndex==-1){
+            endIndex=str.length;
+        }
+        var neirong=str.slice(startIndex,endIndex)
+        var result=neirong.split("=")[1]
+        return result;
+    }
+        // console.log(getCookie("sex"))
+
+    //setCookie函数传两个参数：一个参数为对象(包含属性名和属性值),一个为生命周期的日期为Number类型
+    function setCookie(neirong,day){
+        var date =new Date()
+        date.setDate(day)
+        for(var i in neirong){
+            document.cookie=i + "=" + neirong[i] + ";"+"expires="+date;
+        }   
+    }
+    // setCookie({secondname:"linn"},15)
+
+    //removeCookie函数传一个cookie的name是string类型
+    function removeCookie(cookieName){
+        var date=new Date()
+        date.setDate(date.getDate()-1)
+        document.cookie=cookieName+"=q;expires="+date;
+    }
+    //removeCookie("secondname")	
+//8.弹出框插件
+		
